110093bd2aa5f5c496842543b0139f6c4b5e68d7
doddyaditya <13517008@std.stei.itb.ac.id>
2019-11-17 16:53:00 +0700

    add transaction history

--- bank_pro/src/components/Transaction/TransactionHistory.js
+++ bank_pro/src/components/Transaction/TransactionHistory.js
@@ -1,70 +1,181 @@
-import React from "react";
+import React, { Component } from "react";
+import produce from "immer";
 import "./TransactionHistory.css";
 import NavigationBar from "../NavigationBar/NavigationBar";
+import Cookies from "universal-cookie";
 
 /**
- * Merupakan laman Help yang memberikan basa-basi mengenai fitur yang terdapat dalam program ini
+ * Merupakan laman history transaksi
  */
-const TransactionHistory = () => {
-  return (
-    <React.Fragment>
-      <NavigationBar/>
-        <div className="help-wrapper">
-          <div className="help-margin">
-            <div className="help-title">
-              <b>HELP</b>
-            </div>
-            <div className="help-text">
-              <p>
-                <b>REGISTER</b>
-              </p>
-              <p>
-                Enter your desired username and password at the provided input box.
-                After that, click the register button It may have been taken by
-                others though, but usually it haven't been taken by others, so
-                usually you won't have to rethink for your username and password.
-                After being registered, you can access your log into your account so
-                that you can start <i>stalking</i>.
-              </p>
-              <p>
-                <b>LOGIN</b>
-              </p>
-              <p>
-                Log into your account that you have been registered. You can input
-                your username and your password at the provided space and hit the
-                login button. After logging in, you now gain access to the API and
-                now can start <i>stalking</i>. Remember to register first if you
-                don't have an account. Otherwise, you won't be able to login (
-                <i>obviously</i>).
-              </p>
-              <p>
-                <b>SEARCH</b>
-              </p>
-              <p>
-                Your stalking machine! Be it your friend's NIM or name, this machine
-                can look up the details for you! Just enter your query at the
-                provided space and click on the search button to start searching.
-                This machine will then return the result it gets from the API, which
-                is the list of name and the details about that person which has the
-                same criteria as your query (or nothing if it doesn't match with
-                anything). Do remember, this feature is only accessible once you've
-                logged in.
-              </p>
-              <p>
-                <b>LOGOUT</b>
-              </p>
-              <p>
-                Log out from your account. This feature is only accessible once
-                you've logged in. Though I should say you shouldn't use this feature
-                because it's preventing you from stalking your friends. I suggest
-                you should only use this feature once doctors and psychiatrists come
-                into your house because you've done too much stalking.
-              </p>
+class TransactionHistory extends Component{
+  state = {
+    data : [],
+    balance : [],
+    virtual : [],
+    transaksi : [],
+  }
+
+  componentWillMount() {
+    const cookie = new Cookies();
+    const account = Object.values(cookie.get("login"))[0].split(";")[0];
+    // Sending request using SOAP to ws-bank
+    const request = require("request");
+    let xml = 
+    `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://service/">
+        <soapenv:Header/>
+        <soapenv:Body>
+            <ser:GetVirtualAccount/>
+        </soapenv:Body>
+    </soapenv:Envelope>`;
+    let xml2 = 
+    `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://service/">
+        <soapenv:Header/>
+        <soapenv:Body>
+          <ser:AccountHistory>
+              <arg0>` + account + `</arg0>
+          </ser:AccountHistory>
+        </soapenv:Body>
+    </soapenv:Envelope>>`;
+
+    // Target wsdl
+    var options = {
+      url: 'http://localhost:8080/ws-bank/service/TransactionHistory?wsdl',
+      method: 'POST',
+      body: xml,
+      headers: {
+        'Content-Type':'text/xml;charset=utf-8',
+      }
+    };
+
+    var options2 = {
+      url: 'http://localhost:8080/ws-bank/service/TransactionHistory?wsdl',
+      method: 'POST',
+      body: xml2,
+      headers: {
+        'Content-Type':'text/xml;charset=utf-8',
+      }
+    };
+
+    //Method melakukan perbandingan nilai
+    const comparator = (a,b) => {
+      if (a[0] < b[0]) return -1;
+      if (a[0] > b[0]) return 1;
+      return 0;
+    }
+
+    const sortByDate = () => {
+      let newtransaksi = this.state.transaksi;
+      newtransaksi = this.state.transaksi.slice().sort(comparator);
+      this.setState({transaksi : newtransaksi});
+    };
+
+    //Method untuk melakukan perubahan state transaksi dan menentukan jenis transaksi
+    const changeData = account => produce(this.state, draft => {
+      for(var i = 0; i < draft.data.length; i++){
+        if(draft.data[i][3] === account){
+          draft.transaksi[i][3] = draft.data[i][2];
+        } else {
+          for(var j = 0; j < draft.virtual.length; j++){
+            if(draft.data[i][3] === draft.virtual[j][1]){
+              if(draft.virtual[j][0] === account){
+                draft.transaksi[i][3] = draft.data[i][2];
+              }
+              else{
+                draft.transaksi[i][3] = draft.virtual[j][0];
+              }
+            }
+          }
+        }
+        if(draft.transaksi[i][2] === account){
+          draft.transaksi[i].push("DEBIT");
+        } else {
+          draft.transaksi[i].push("KREDIT");
+        }
+      }
+    });
+
+    // Get the callback from the result
+    let callback = (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        // Get return dan set state virtual
+        let result = new DOMParser().parseFromString(body, 'text/xml');
+        var nodeList = result.getElementsByTagName('return');
+        let temp_data = [];
+        for(var i = 0; i < nodeList.length; i++){
+          let temp_element = nodeList[i];
+          let child = [];
+          child.push(temp_element.getElementsByTagName('accountnumber')[0].textContent);
+          child.push(temp_element.getElementsByTagName('virtualnumber')[0].textContent);
+          temp_data.push(child);
+        }
+        this.setState({virtual : temp_data});
+        request(options2, callback2);
+      }
+    };
+    
+    // Get the callback from the result
+    let callback2 = (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        // Get return dan set state data, balance, transaksi serta memanggil sortByDate()
+        let result = new DOMParser().parseFromString(body, 'text/xml');
+        var nodeList = result.getElementsByTagName('return');
+        let temp_data = [];
+        for(var i = 0; i < nodeList.length; i++){
+          let temp_element = nodeList[i];
+          let child = [];
+          child.push(temp_element.getElementsByTagName('transactiontime')[0].textContent);
+          child.push(temp_element.getElementsByTagName('amount')[0].textContent);
+          child.push(temp_element.getElementsByTagName('accountnumber')[0].textContent);
+          child.push(temp_element.getElementsByTagName('targetaccount')[0].textContent);
+          temp_data.push(child);
+        }
+        let temp_acc = [];
+        temp_acc.push(nodeList[0].getElementsByTagName('balance')[0].textContent);
+        this.setState({data: temp_data, balance: temp_acc, transaksi: temp_data});
+        this.setState(changeData(account));
+        sortByDate();
+      }
+    };
+    
+    request(options, callback);
+  };
+  
+  render() {
+    return (
+      <React.Fragment>
+        <NavigationBar/>
+        <div className="overflow-auto">
+          <div className="history-wrapper" >
+            <div className="table-responsive" id="th">
+              <div className="account-balance">
+                <h5>{"Your Balance is IDR "}{this.state.balance}</h5>
+              </div>
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Waktu Transaksi</th>
+                    <th scope="col">Jenis Transaksi</th>
+                    <th scope="col">Jumlah Transaksi</th>
+                    <th scope="col">No. Rekening Terkait</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    {this.state.transaksi.map(res =>(
+                      <tr key={res[0]}>
+                        <td>{res[0]}</td>
+                        <td>{res[4]}</td>
+                        <td>{res[1]}</td>
+                        <td>{res[3]}</td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
-    </React.Fragment>
-  );
-};
+      </React.Fragment>
+    );
+  }
+}
 
 export default TransactionHistory;
