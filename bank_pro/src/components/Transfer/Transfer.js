cad536c8baea83981d13858b8b3a25ae84b3984a
Aries Tri Sutrisno KA <13515116@std.stei.itb.ac.id>
2019-11-18 13:32:32 +0700

    Transfer completed

--- bank_pro/src/components/Transfer/Transfer.js
+++ bank_pro/src/components/Transfer/Transfer.js
@@ -1,70 +1,134 @@
-import React from "react";
-import "./Transfer.css";
+import React, { Component } from "react";
 import NavigationBar from "../NavigationBar/NavigationBar";
-
+import Cookies from "universal-cookie";
+import "./Transfer.css";
+import TransferForm from "./TransferForm";
 /**
- * Merupakan laman Help yang memberikan basa-basi mengenai fitur yang terdapat dalam program ini
+ * Merupakan laman Transfer yang memberikan basa-basi mengenai fitur yang terdapat dalam program ini
  */
-const Transfer = () => {
-  return (
-    <React.Fragment>
-      <NavigationBar/>
-    <div className="help-wrapper">
-      <div className="help-margin">
-        <div className="help-title">
-          <b>HELP</b>
-        </div>
-        <div className="help-text">
-          <p>
-            <b>REGISTER</b>
-          </p>
-          <p>
-            Enter your desired username and password at the provided input box.
-            After that, click the register button It may have been taken by
-            others though, but usually it haven't been taken by others, so
-            usually you won't have to rethink for your username and password.
-            After being registered, you can access your log into your account so
-            that you can start <i>stalking</i>.
-          </p>
-          <p>
-            <b>LOGIN</b>
-          </p>
-          <p>
-            Log into your account that you have been registered. You can input
-            your username and your password at the provided space and hit the
-            login button. After logging in, you now gain access to the API and
-            now can start <i>stalking</i>. Remember to register first if you
-            don't have an account. Otherwise, you won't be able to login (
-            <i>obviously</i>).
-          </p>
-          <p>
-            <b>SEARCH</b>
-          </p>
-          <p>
-            Your stalking machine! Be it your friend's NIM or name, this machine
-            can look up the details for you! Just enter your query at the
-            provided space and click on the search button to start searching.
-            This machine will then return the result it gets from the API, which
-            is the list of name and the details about that person which has the
-            same criteria as your query (or nothing if it doesn't match with
-            anything). Do remember, this feature is only accessible once you've
-            logged in.
-          </p>
-          <p>
-            <b>LOGOUT</b>
-          </p>
-          <p>
-            Log out from your account. This feature is only accessible once
-            you've logged in. Though I should say you shouldn't use this feature
-            because it's preventing you from stalking your friends. I suggest
-            you should only use this feature once doctors and psychiatrists come
-            into your house because you've done too much stalking.
-          </p>
+
+class Transfer extends Component {
+  constructor() {
+    super();
+    const cookie = new Cookies();
+    this.state.cookieId = cookie.get("login").split(";")[0];
+  }
+
+  state = {
+    trfStatus: undefined,
+    trfMessage: "Error not loaded."
+  };
+  
+  handleTrf = async e => {
+    // Handles Login
+    e.preventDefault();
+
+    // Getting transfer details
+    const senderAcc = e.target.elements.sender_acc.value;
+    const receiverAcc = e.target.elements.receiver_acc.value;
+    const trfAmount = e.target.elements.trf_amount.value;
+    
+    // Sending request using SOAP to ws-bank
+    const request = require("request");
+    let xml = 
+    `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://service/">
+        <soapenv:Header/>
+        <soapenv:Body>
+          <ser:TransferResult>
+              <!--Optional:-->
+              <arg0>` + senderAcc + `</arg0>
+              <!--Optional:-->
+              <arg1>` + receiverAcc + `</arg1>
+              <!--Optional:-->
+              <arg2>` + trfAmount + `</arg2>
+          </ser:TransferResult>
+        </soapenv:Body>
+    </soapenv:Envelope>`;
+
+    // Target wsdl
+    var options = {
+      url: 'http://localhost:8080/ws-bank/service/Transfer?wsdl',
+      method: 'POST',
+      body: xml,
+      headers: {
+        'Content-Type':'text/xml;charset=utf-8',
+      }
+    };
+
+    // Get the callback from the result
+    let callback = (error, response, body) => {
+      console.log(body);
+      if (!error && response.statusCode === 200) {
+        // Get return 
+        let result = new DOMParser().parseFromString(body, 'text/xml');
+        result = result.getElementsByTagName('return')[0];
+        
+        this.setState({trfStatus: result.childNodes[0].textContent})
+
+        if (this.state.trfStatus === "2002") {
+          this.setState({
+            trfMessage: "Transfer Success."
+          })
+          // window.location.reload();
+        }
+        else if (this.state.trfStatus === "2000"){
+          this.setState({trfMessage: "Failed to update your account's balance."})
+        }
+        else if (this.state.trfStatus === "2001"){
+          this.setState({trfMessage: "Failed to update receiver account's balance."})
+        }
+        else if (this.state.trfStatus === "4001"){
+          this.setState({trfMessage: "Receiver account/virtual number is invalid."})
+        }
+        else if (this.state.trfStatus === "4002"){
+          this.setState({trfMessage: "Insufficient balance."})
+        }
+        else if (this.state.trfStatus === "4003"){
+          this.setState({trfMessage: "Failed to get your account's balance."})
+        }
+        else if (this.state.trfStatus === "4004"){
+          this.setState({trfMessage: "Failed to update your account's data."})
+        }
+        else if (this.state.trfStatus === "4005"){
+          this.setState({trfMessage: "Failed to update receiver account's data."})
+        }
+        else if (this.state.trfStatus === "4006"){
+          this.setState({trfMessage: "Failed to record transaction details."})
+        }
+        else if (this.state.trfStatus === "4007"){
+          this.setState({trfMessage: "Failed to get your account's balance. (2)"})
+        }
+        else {
+          console.log("out");
+          this.setState({
+            trfStatus: false,
+            status: "Transfer Failed."
+          });
+        }
+      };
+    };
+
+    request(options, callback);
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <NavigationBar/>
+      <div className="trf-wrapper">
+        <div className="trf-margin">
+          <div className="trf-title">
+            <b>TRANSFER</b>
+          </div>
+          <div className="trf-text">
+          </div>
+          <TransferForm onTrf={this.handleTrf} trfst={this.state.trfMessage} senderNo={this.state.cookieId} />
         </div>
       </div>
-    </div>
-    </React.Fragment>
-  );
-};
+      
+      </React.Fragment>
+    );
+  };
+}
 
 export default Transfer;
